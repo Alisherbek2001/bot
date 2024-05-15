807a630804a0788733e349eeebe9e88390d597fe
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-16 00:11:51 +0500

    router added

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,6 +1,11 @@
 from aiogram import Dispatcher
+
+from .my_company import router as company_router
+from .my_order import router as order_router
 from .start import router as start_routes
 
 
 def register_routes(dp: Dispatcher):
     dp.include_router(start_routes)
+    dp.include_router(order_router)
+    dp.include_router(company_router)
